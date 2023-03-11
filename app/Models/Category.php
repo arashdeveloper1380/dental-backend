af2b0884197a0cbe6935b7253400f2c30ca1435d
arashdeveloper1380 <arash.developer1380@gmail.com>
2023-03-11 10:55:10 +0330

    build blog and categories migration

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table="categories";
+
+    protected $fillable = ['name','meta_desc','meta_keywords'];
+
+    public function blog(){
+        return $this->hasMany(Blog::class);
+    }
+}
