f2f048e08772c6df33fac85e3f8e2e46f1d9d36a
arashdeveloper1380 <arash.developer1380@gmail.com>
2023-10-03 10:25:23 +0330

    fix create nobat

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -45,4 +45,21 @@ class Controller extends BaseController
 //    public function blogByCategory($value){
 //        return
 //    }
+
+    public static function send_sms($receptor, $message = null,$template = null, $token = null, $token1 = null, $token2 = null){
+        try {
+            $api = new KavenegarApi('486D4A4C684C30436B79522F4A564E6647773445706446666775314A6747512F654749324E4639336854453D');
+            $result = $api->VerifyLookup($receptor, $token, $token1, $token2, $template);
+            $sender = "10001000700100";
+            $result = $api->Send($sender, $receptor, $message);
+            if($result){
+                return 'ok';
+            }
+        } catch (\Kavenegar\Exceptions\ApiException $e){
+            return $e->errorMessage();
+        } catch (\Kavenegar\Exceptions\HttpException $e) {
+            // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+            return $e->errorMessage();
+        }
+    }
 }
--- app/Http/Controllers/admin/NobatController.php
+++ app/Http/Controllers/admin/NobatController.php
@@ -23,11 +23,87 @@ class NobatController extends Controller
         return view('admin.nobat.create');
     }
 
-    public function store(Request $request){
+//     public function store(Request $request){
+//         $date = date('Y/m/d'); // تاریخ امروز
+//         $start_time = strtotime('9:00 AM'); // ساعت شروع
+//         $end_time = strtotime('8:00 PM'); // ساعت پایان
+//         $time_slots = [];
+
+//         for ($i = 0; $i < 7; $i++) { // حلقه برای 7 روز در هفته
+//             $current_date = date('Y/m/d', strtotime("+$i days")); // تاریخ هر روز
+//             $current_date = verta($current_date)->format('Y/m/d');
+
+//             // اگر هفته قبلی به پایان رسیده است، هفته جدید را بسازید
+//             if ($current_date > $date) {
+//                 $date = $current_date;
+//                 $start_time = strtotime('9:00 AM');
+//                 $end_time = strtotime('8:00 PM');
+//             }
+
+//             $current_time = $start_time;
+
+//             while ($current_time <= $end_time) { // حلقه برای ساعت های مورد نظر
+//                 $time_slot = date('H:i', $current_time); // تبدیل ساعت به فرمت مناسب
+
+//                 $existing_nobat = Nobat::where('date', $current_date)
+//                 ->whereJsonContains('time', $time_slot)
+//                 ->first();
+
+//                 if (!$existing_nobat) {
+//                     array_push($time_slots, $time_slot);
+//                 }
+
+//                 $current_time += 3600;
+
+//                 // array_push($time_slots, $time_slot);
+//                 // $current_time += 3600; // اضافه کردن یک ساعت به ساعت فعلی
+//             }
+
+//             $end_date = Nobat::query()->orderByDesc('date')->first();
+
+//             if($end_date == null){
+//                 Nobat::create([
+//                     'date' => $current_date,
+//                     'time' => $time_slots,
+//                     'user_id' => Auth::user()->id,
+//                 ]);
+//             }else if($current_date < $end_date && Nobat::query()->count() == 7){
+//                 return redirect()->route('nobat.index')->with('error','بعد از تمام شدن هفته دکمه ثبت وقت باز میشود ');
+//             }
+//             Nobat::create([
+//                 'date' => $current_date,
+//                 'time' => $time_slots,
+//                 'user_id' => Auth::user()->id,
+//             ]);
+
+//         }
+// //        Nobat::create([
+// //            'date'      => $request->get('date'),
+// //            'time'      => [
+// //                'ساعت 9',
+// //                'ساعت 10',
+// //                'ساعت 11',
+// //                'ساعت 12',
+// //                'ساعت 13',
+// //                'ساعت 14',
+// //                'ساعت 15',
+// //                'ساعت 16',
+// //                'ساعت 17',
+// //                'ساعت 18',
+// //                'ساعت 19',
+// //                'ساعت 20',
+// //            ],
+// //            'user_id'   => Auth::user()->id,
+// //        ]);
+// //
+// //        return redirect()->route('nobat.index')->with('success','وقت نوبت با موفقیت ثبت شد...');
+//     }
+
+    public function store(Request $request)
+    {
         $date = date('Y/m/d'); // تاریخ امروز
         $start_time = strtotime('9:00 AM'); // ساعت شروع
         $end_time = strtotime('8:00 PM'); // ساعت پایان
-        $time_slots = [];
 
         for ($i = 0; $i < 7; $i++) { // حلقه برای 7 روز در هفته
             $current_date = date('Y/m/d', strtotime("+$i days")); // تاریخ هر روز
@@ -41,50 +117,41 @@ class NobatController extends Controller
             }
 
             $current_time = $start_time;
+            $time_slots = [];
 
             while ($current_time <= $end_time) { // حلقه برای ساعت های مورد نظر
                 $time_slot = date('H:i', $current_time); // تبدیل ساعت به فرمت مناسب
-                array_push($time_slots, $time_slot);
+
+                // بررسی تکراری نبودن ساعت
+                $existing_nobat = Nobat::where('date', $current_date)
+                    ->whereJsonContains('time', $time_slot)
+                    ->first();
+
+                if (!$existing_nobat) {
+                    array_push($time_slots, $time_slot);
+                }
+
                 $current_time += 3600; // اضافه کردن یک ساعت به ساعت فعلی
             }
+
             $end_date = Nobat::query()->orderByDesc('date')->first();
 
-            if($end_date == null){
+            if ($end_date == null) {
                 Nobat::create([
                     'date' => $current_date,
                     'time' => $time_slots,
                     'user_id' => Auth::user()->id,
                 ]);
-            }else if($current_date < $end_date && Nobat::query()->count() == 7){
-                return redirect()->route('nobat.index')->with('error','بعد از تمام شدن هفته دکمه ثبت وقت باز میشود ');
+            } else if ($current_date < $end_date && Nobat::query()->count() == 7) {
+                return redirect()->route('nobat.index')->with('error', 'بعد از تمام شدن هفته دکمه ثبت وقت باز میشود');
             }
+
             Nobat::create([
                 'date' => $current_date,
                 'time' => $time_slots,
                 'user_id' => Auth::user()->id,
             ]);
-
         }
-//        Nobat::create([
-//            'date'      => $request->get('date'),
-//            'time'      => [
-//                'ساعت 9',
-//                'ساعت 10',
-//                'ساعت 11',
-//                'ساعت 12',
-//                'ساعت 13',
-//                'ساعت 14',
-//                'ساعت 15',
-//                'ساعت 16',
-//                'ساعت 17',
-//                'ساعت 18',
-//                'ساعت 19',
-//                'ساعت 20',
-//            ],
-//            'user_id'   => Auth::user()->id,
-//        ]);
-//
-//        return redirect()->route('nobat.index')->with('success','وقت نوبت با موفقیت ثبت شد...');
     }
 
     public function destroy($id){
